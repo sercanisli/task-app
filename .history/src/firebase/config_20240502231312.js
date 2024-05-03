5f4cea0b6fc0e8b6e9bf3174e0a79e9ee84750b2
Sercan ISLI <sercanislii@gmail.com>
2024-05-03 18:48:50 +0300

    Firebase Authentication added to project.

--- .history/src/firebase/config_20240502231312.js
+++ .history/src/firebase/config_20240502231312.js
@@ -13,8 +13,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const projectAuth = firebase.auth();
 const projectFirestore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectAuth ,projectFirestore, timestamp};
+export {projectFirestore, timestamp};

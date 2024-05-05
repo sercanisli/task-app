adc0a78d707de98d617c8aeee1ba7c6c7d85cd78
Sercan ISLI <sercanislii@gmail.com>
2024-05-05 03:23:22 +0300

    useLogout implemented to Navbar component.

---
+++ .history/src/components/Navbar_20240505032054.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="main-nav">
+      <router-link :to="{ name: 'home' }">Projects</router-link>
+      <router-link :to="{ name: 'AddProject' }">Add New Project</router-link>
+      <button>Logout</button>
+    </nav>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+
+  import useLogout from '../composables/useLogout';
+
+  export default {
+    setup() {
+      const {logout, error} = useLogout();
+      const handleClick = async () => {
+        await logout();
+        if(!error.value){
+          
+        }
+      }
+      return {handleClick}
+    }
+  }
+  
+  <style>
+  .main-nav {
+    text-align: center;
+    margin: 40px auto;
+  }
+  .main-nav a {
+    display: inline-block;
+    text-decoration: none;
+    margin: 0 10px;
+    color: #999;
+    font-size: 18px;
+  }
+  a.router-link-active {
+    border-bottom: 2px solid #76dd78;
+    padding-bottom: 4px;
+    color: #76dd78;
+  }
+  </style>
---
+++ .history/src/components/Navbar_20240505032231.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav class="main-nav">
+      <router-link :to="{ name: 'home' }">Projects</router-link>
+      <router-link :to="{ name: 'AddProject' }">Add New Project</router-link>
+      <button class="button" @click="handleClick">Logout</button>
+    </nav>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+
+  import useLogout from '../composables/useLogout';
+
+  export default {
+    setup() {
+      const {logout, error} = useLogout();
+      const handleClick = async () => {
+        await logout();
+        if(!error.value){
+
+        }
+      }
+      return {handleClick}
+    }
+  }
+  
+  <style>
+  .main-nav {
+    text-align: center;
+    margin: 40px auto;
+  }
+  .main-nav a {
+    display: inline-block;
+    text-decoration: none;
+    margin: 0 10px;
+    color: #999;
+    font-size: 18px;
+  }
+  a.router-link-active {
+    border-bottom: 2px solid #76dd78;
+    padding-bottom: 4px;
+    color: #76dd78;
+  }
+  button {
+  text-decoration: none;
+  background: #33a464;
+  color: #fff;
+  font-weight: bold;
+  border: 0;
+  border-radius: 20px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+  </style>
